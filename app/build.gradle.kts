8a58f77365e85612ca839a94e73de36091c84ad3
John <cucosandreiioan@gmail.com>
2026-09-04 13:41:43 +0300

    chore(release): bump Android versionCode to 8 (v2.0.1) and generate localized release notes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.pdfchemy.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "2.0.0"
+        versionCode = 8
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
